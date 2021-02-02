476f09f180e79035d71a0b604b9ad2ff4d276b0d
remi.vezy <VEZY@users.noreply.github.com>
2021-02-02 11:57:24 +0100

    Fix test error in gs.jl

--- test/gs.jl
+++ test/gs.jl
@@ -15,16 +15,16 @@ Gs = Medlyn(0.03,12.0) # Instance of a Medlyn type with g0 = 0.03 and g1 = 0.1
 end;
 
 
-VPD = 0.1:0.1:3.0
-plot(x -> gs(Gs,A,Cₛ,VPD = x), VPD, xlabel = "VPD (kPa)",
-            ylab = "Gs (μmol[CO₂] m⁻² s⁻¹)",
-            label = "A = 20, Cₛ = 300.0, g0 = 0.03, g1 = 12.0",
-            legend = :topright)
-plot!(x -> gs(Medlyn(0.03,15.0),A,Cₛ,VPD = x), VPD,
-    label = "A = 20, Cₛ = 300.0, g0 = 0.03, g1 = 15.0")
-plot!(x -> gs(Medlyn(0.01,12.0),A,Cₛ,VPD = x), VPD,
-    label = "A = 20, Cₛ = 300.0, g0 = 0.01, g1 = 12.0")
-plot!(x -> gs(Gs,15,Cₛ,VPD = x), VPD,
-    label = "A = 15, Cₛ = 300.0, g0 = 0.03, g1 = 12.0")
-plot!(x -> gs(Gs,A,200,VPD = x), VPD,
-        label = "A = 20, Cₛ = 200.0, g0 = 0.01, g1 = 12.0")
+# VPD = 0.1:0.1:3.0
+# plot(x -> gs(Gs,A,Cₛ,VPD = x), VPD, xlabel = "VPD (kPa)",
+#             ylab = "Gs (μmol[CO₂] m⁻² s⁻¹)",
+#             label = "A = 20, Cₛ = 300.0, g0 = 0.03, g1 = 12.0",
+#             legend = :topright)
+# plot!(x -> gs(Medlyn(0.03,15.0),A,Cₛ,VPD = x), VPD,
+#     label = "A = 20, Cₛ = 300.0, g0 = 0.03, g1 = 15.0")
+# plot!(x -> gs(Medlyn(0.01,12.0),A,Cₛ,VPD = x), VPD,
+#     label = "A = 20, Cₛ = 300.0, g0 = 0.01, g1 = 12.0")
+# plot!(x -> gs(Gs,15,Cₛ,VPD = x), VPD,
+#     label = "A = 15, Cₛ = 300.0, g0 = 0.03, g1 = 12.0")
+# plot!(x -> gs(Gs,A,200,VPD = x), VPD,
+#         label = "A = 20, Cₛ = 200.0, g0 = 0.01, g1 = 12.0")
