cc3f5f2211490ce79ddf3c2f32bbd21f7164ebc8
remi.vezy <VEZY@users.noreply.github.com>
2021-09-06 18:56:38 +0200

    Add more docs for Weather.

--- src/structs/atmosphere.jl
+++ src/structs/atmosphere.jl
@@ -86,52 +86,90 @@ function Atmosphere(;
 end
 
 """
+    Weather(D <: AbstractArray{<:AbstractAtmosphere}[, S])
+    Weather(df::DataFrame[, mt])
+
+Defines the weather, *i.e.* the local conditions of the Atmosphere for one or more time-steps.
+Each time-step is described using the [`Atmosphere`](@ref) structure.
+
+The simplest way to instantiate a `Weather` is to use a `DataFrame` as input.
+
+The `DataFrame` should be formated such as each row is an observation for a given time-step
+and each column is a variable. The column names should match exactly the field names of the
+[`Atmosphere`](@ref) structure, `i.e.`:
+
+```@example
+fieldnames(Atmosphere)
+```
+
+## See also
+
+- the [`Atmosphere`](@ref) structure
+- the [`read_weather`](@ref) function to read Archimed-formatted meteorology data.
+
+## Examples
 
 ```julia
+# Example of weather data defined by hand (cumbersome):
 w = Weather(
     [
         Atmosphere(T = 20.0, Wind = 1.0, P = 101.3, Rh = 0.65),
         Atmosphere(T = 23.0, Wind = 1.5, P = 101.3, Rh = 0.60),
         Atmosphere(T = 25.0, Wind = 3.0, P = 101.3, Rh = 0.55)
-    ]
+    ],
+    (
+        site = "Test site",
+        important_metadata = "this is important and will be attached to our weather data"
     )
+)
+
+# Example using a DataFrame, that you would usually import from a file:
+using CSV, DataFrames
+file = joinpath(dirname(dirname(pathof(PlantBiophysics))),"test","inputs","meteo.csv")
+df = CSV.read(file, DataFrame; header=5, datarow = 6)
+# Select and rename the variables:
+select!(df, :date, :VPD, :temperature => :T, :relativeHumidity => :Rh, :wind => :Wind, :atmosphereCO2_ppm => :Cₐ)
+df[!,:duration] = 1800 # Add the time-step duration, 30min
+
+# Make the weather, and add some metadata:
+Weather(df, (site = "Aquiares", file = file))
 ```
 """
-struct Weather{D <: AbstractArray{<:AbstractAtmosphere}, S <: MutableNamedTuple}
+struct Weather{D <: AbstractArray{<:AbstractAtmosphere},S <: MutableNamedTuple}
     data::D
     metadata::S
 end
 
 function Weather(df::T) where T <: AbstractArray{<:AbstractAtmosphere}
-    Weather(df,MutableNamedTuple())
+    Weather(df, MutableNamedTuple())
 end
 
-function Weather(df::T, mt::S) where {T <: AbstractArray{<:AbstractAtmosphere}, S <:NamedTuple}
-    Weather(df,MutableNamedTuple(), MutableNamedTuple(;mt...))
+function Weather(df::T, mt::S) where {T <: AbstractArray{<:AbstractAtmosphere},S <: NamedTuple}
+    Weather(df, MutableNamedTuple(), MutableNamedTuple(;mt...))
 end
 
 function Weather(df::DataFrame, mt::S) where S <: MutableNamedTuple
-    Weather([Atmosphere(; i...) for i in eachrow(df)],mt)
+    Weather([Atmosphere(; i...) for i in eachrow(df)], mt)
 end
 
 function Weather(df::DataFrame, mt::S) where S <: NamedTuple
     mt = MutableNamedTuple(;mt...)
-    Weather([Atmosphere(; i...) for i in eachrow(df)],mt)
+    Weather([Atmosphere(; i...) for i in eachrow(df)], mt)
 end
 
 function Weather(df::DataFrame, dict::S) where S <: AbstractDict
     # There must be a better way for transforming a Dict into a MutableNamedTuple...
-    Weather(df,MutableNamedTuple(; NamedTuple{Tuple(Symbol.(keys(dict)))}(values(dict))...))
+    Weather(df, MutableNamedTuple(; NamedTuple{Tuple(Symbol.(keys(dict)))}(values(dict))...))
 end
 
 function Weather(df::DataFrame)
-    Weather(df,MutableNamedTuple())
+    Weather(df, MutableNamedTuple())
 end
 
 function Base.show(io::IO, n::Weather)
-    printstyled(io,"Weather data.\n", bold = true, color = :green)
-    printstyled(io,"Metadata: `$(NamedTuple(n.metadata))`.\n", color = :cyan)
-    printstyled(io,"Data:\n", color = :green)
+    printstyled(io, "Weather data.\n", bold = true, color = :green)
+    printstyled(io, "Metadata: `$(NamedTuple(n.metadata))`.\n", color = :cyan)
+    printstyled(io, "Data:\n", color = :green)
     # :normal, :default, :bold, :black, :blink, :blue, :cyan, :green, :hidden, :light_black, :light_blue, :light_cyan, :light_green, :light_magenta, :light_red, :light_yellow, :magenta, :nothing, :red,
 #   :reverse, :underline, :white, or :yellow
     print(DataFrame(n))
@@ -148,7 +186,7 @@ Transform a Weather type into a DataFrame.
 See also [`Weather`](@ref) to make the reverse.
 """
 function DataFrame(data::Weather)
-    return DataFrame(data.data)
+return DataFrame(data.data)
 end
 
 """
@@ -168,7 +206,7 @@ Saturated water vapour pressure (es, in kPa) at given temperature `T` (°C).
 See Jones (1992) p. 110 for the equation.
 """
 function e_sat(T)
-  0.61375 * exp((17.502 * T) / (T + 240.97))
+0.61375 * exp((17.502 * T) / (T + 240.97))
 end
 
 """
@@ -245,7 +283,7 @@ edited by John L. Monteith et Mike H. Unsworth, 217‑47. Boston: Academic Press
 
 """
 function psychrometer_constant(P, λ, Cₚ, ε)
-    γ = (Cₚ * P)/(ε * λ)
+    γ = (Cₚ * P) / (ε * λ)
     return γ
 end
 
@@ -304,10 +342,10 @@ end
 if not provided (see [`Constants`](@ref)).
 
 """
-function latent_heat_vaporization(Tₐ,λ₀)
+function latent_heat_vaporization(Tₐ, λ₀)
   λ₀ - 2.365e3 * Tₐ
 end
 
 function latent_heat_vaporization(Tₐ)
-    latent_heat_vaporization(Tₐ,Constants().λ₀)
+    latent_heat_vaporization(Tₐ, Constants().λ₀)
 end
