077b46b5102b10aab99d434cd7784b091ac66a99
SimonTreillou <simontreillou@orange.fr>
2022-05-15 16:44:21 +0200

    homogenize Gs

--- src/io/read_licor6400.jl
+++ src/io/read_licor6400.jl
@@ -5,7 +5,7 @@ Import Licor6400 data (such as Medlyn 2001 data).
 
 """
 function read_licor6400(file)
-    df = CSV.read(file, DataFrame, header = 1, datarow = 3)
+    df = CSV.read(file, DataFrame, header = 1, skipto = 3)
 
     if hasproperty(df, :Ttop)
         rename!(df, :Ttop => :Tmin)
@@ -18,16 +18,19 @@ function read_licor6400(file)
     # Renaming variables to fit the standard in the package:
     rename!(
         df,
-        :Cond => :gs, :CO2S => :Cₐ, :Tair => :T, :Press => :P, :RH_S => :Rh,
+        :Cond => :Gₛ, :CO2S => :Cₐ, :Tair => :T, :Press => :P, :RH_S => :Rh,
         :PARi => :PPFD, :Ci => :Cᵢ, :Tleaf => :Tₗ,
         :VpdL => :Dₗ, :Photo => :A, :BLCond => :Gbv,
     )
 
     # Recomputing the variables to fit the units used in the package:
-    df[!,:VPD] = df[!,:Dₗ]
-    df[!,:gs] = round.(gsw_to_gsc.(df[:,:gs]), digits = 5)
-    df[!,:AVPD] = df[:,:A] ./ (df[:,:Cₐ] .* sqrt.(df[:,:Dₗ]))
     df[!,:Rh] = df[!,:Rh] ./ 100.0
+    transform!(
+		df, 
+		[:T,:Rh] => ((x,y) -> e_sat.(x) .- vapor_pressure.(x, y)) => :VPD,
+		:Gₛ => (x -> gsw_to_gsc.(x)) => :Gₛ,
+		[:A,:Cₐ,:Dₗ] => ((x,y,z) -> x ./ (y .* sqrt.(z))) => :AVPD,
+	)
 
     return df
 end
