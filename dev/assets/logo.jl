16833ebac380763df3ec902bf929cb069942d800
Documenter.jl <documenter@juliadocs.github.io>
2022-12-17 21:41:40 +0000

    build based on 2a0b9ec

--- dev/assets/logo.jl
+++ dev/assets/logo.jl
@@ -1,13 +1,13 @@
-using PlantBiophysics, MultiScaleTreeGraph, PlantGeom, CairoMakie, Dates
+using PlantBiophysics, MultiScaleTreeGraph, PlantGeom, CairoMakie, Dates, PlantMeteo
 
 mtg = read_opf(joinpath(dirname(dirname(pathof(PlantBiophysics))), "test", "inputs", "scene", "opf", "coffee.opf"))
 weather = read_weather(
-    joinpath(dirname(dirname(pathof(PlantBiophysics))), "test", "inputs", "meteo.csv"),
+    joinpath(dirname(dirname(pathof(PlantMeteo))), "test", "data", "meteo.csv"),
     :temperature => :T,
     :relativeHumidity => (x -> x ./ 100) => :Rh,
     :wind => :Wind,
     :atmosphereCO2_ppm => :Cₐ,
-    date_format = DateFormat("yyyy/mm/dd")
+    date_format=DateFormat("yyyy/mm/dd")
 )
 models = read_model(joinpath(dirname(dirname(pathof(PlantBiophysics))), "test", "inputs", "models", "plant_coffee.yml"))
 
@@ -16,7 +16,7 @@ transform!(
     [:Ra_PAR_f, :Ra_NIR_f] => ((x, y) -> x + y) => :Rₛ,
     :Ra_PAR_f => (x -> x * 4.57) => :PPFD,
     (x -> 0.3) => :d,
-    ignore_nothing = true
+    ignore_nothing=true
 )
 
 energy_balance!(mtg, models, weather)
@@ -24,12 +24,12 @@ energy_balance!(mtg, models, weather)
 transform!(
     mtg,
     :Tₗ => (x -> x[1]) => :Tₗ_1,
-    ignore_nothing = true
+    ignore_nothing=true
 )
 
-f = Figure(backgroundcolor = :transparent)
-ax = Axis(f[1, 1], backgroundcolor = :transparent)
-viz!(f[1, 1], mtg, color = :Tₗ_1)
+f = Figure(backgroundcolor=:transparent)
+ax = Axis(f[1, 1], backgroundcolor=:transparent)
+viz!(f[1, 1], mtg, color=:Tₗ_1)
 hidespines!(ax)
 hidedecorations!(ax)
 f
