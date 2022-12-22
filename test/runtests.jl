5319eaddcd258fcb1a83e2ba694a6a10d0b0406a
Rémi Vezy <VEZY@users.noreply.github.com>
2022-12-22 13:51:48 +0100

    Update runtests.jl
    Put all tests into a common testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,39 +11,42 @@ using PlantBiophysics
 using PlantBiophysics.PlantMeteo
 using PlantBiophysics.PlantSimEngine
 
-Aqua.test_all(
-    PlantBiophysics,
-    ambiguities=false # Removing this test as dependencies return ambiguities...
-)
+@testset "Testing PlantBiophysics" begin
 
-@testset "File IO" begin
-    include("test-IO.jl")
-end
+    Aqua.test_all(
+        PlantBiophysics,
+        ambiguities=false # Removing this test as dependencies return ambiguities...
+    )
 
-@testset "Structures" begin
-    include("test-structs.jl")
-end
+    @testset "File IO" begin
+        include("test-IO.jl")
+    end
 
-@testset "Temperature dependence" begin
-    include("test-temp-dependence.jl")
-end
+    @testset "Structures" begin
+        include("test-structs.jl")
+    end
 
-@testset "Light interception" begin
-    include("test-beer.jl")
-end
+    @testset "Temperature dependence" begin
+        include("test-temp-dependence.jl")
+    end
 
-@testset "Stomatal conductance" begin
-    include("test-gs.jl")
-end
+    @testset "Light interception" begin
+        include("test-beer.jl")
+    end
 
-@testset "Energy balance" begin
-    include("test-energy_balance.jl")
-end
+    @testset "Stomatal conductance" begin
+        include("test-gs.jl")
+    end
 
-# @testset "MTG compatibility" begin
-#     include("test-mtg.jl")
-# end
+    @testset "Energy balance" begin
+        include("test-energy_balance.jl")
+    end
 
-@testset "Fitting" begin
-    include("test-fitting.jl")
+    # @testset "MTG compatibility" begin
+    #     include("test-mtg.jl")
+    # end
+
+    @testset "Fitting" begin
+        include("test-fitting.jl")
+    end
 end
