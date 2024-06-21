783101b39ad538a150d3c0ff3f83b7e32d6b6964
Rémi Vezy <VEZY@users.noreply.github.com>
2024-06-21 14:38:03 +0200

    Remove Missing model

--- src/PlantBiophysics.jl
+++ src/PlantBiophysics.jl
@@ -61,7 +61,7 @@ include("processes/conductances/boundary/gb.jl")
 
 # Energy balance
 include("processes/energy/longwave_energy.jl")
-include("processes/energy/Missing.jl")
+# include("processes/energy/Missing.jl")
 include("processes/energy/Monteith.jl")
 
 # File IO
