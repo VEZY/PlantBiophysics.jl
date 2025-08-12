faeba7ed1e2e467e72a6fd7ea4bced077f5cb08e
Rémi Vezy <VEZY@users.noreply.github.com>
2025-08-12 10:10:03 +0200

    Update test-IO.jl

--- test/test-IO.jl
+++ test/test-IO.jl
@@ -23,3 +23,18 @@ file = joinpath(dirname(dirname(pathof(PlantBiophysics))), "test", "inputs", "mo
     @test model[3].VcMaxRef == 200.0 # Given in the file
     @test model[3].O₂ == 210.0 # Use default value
 end;
+
+
+@testset "read_walz()" begin
+    absorptance = 0.85
+    file_walz_gfs3000 = joinpath(dirname(dirname(pathof(PlantBiophysics))), "test", "inputs", "data", "P1F20129.csv")
+    data_walz = read_walz(file_walz_gfs3000; abs=absorptance)
+    required_names = [:Dₗ, :Cₐ, :Cᵢ, :A, :Gₛ, :Rh, :VPD, :T, :Tₗ, :P, :aPPFD]
+    @test all(hasproperty(data_walz, name) for name in required_names) # All computed columns are available
+    @test nrow(dropmissing(data_walz[:, required_names])) == nrow(data_walz) # No data is missing
+    @test all(extrema(data_walz.T) .≈ (22.95, 28.08)) # Control that we are in °C
+    @test all(extrema(data_walz.Rh) .≈ (0.3592, 0.7712)) # Control that we are in [0,1]
+    @test all(extrema(data_walz.Rh) .≈ (0.3592, 0.7712)) # Control that we are in [0,1]
+    @test data_walz.aPPFD[1] ≈ absorptance * data_walz.PARtop[1] # Control that we are in μmol m⁻² s⁻¹
+end;
+
